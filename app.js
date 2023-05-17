72b5be3d172cd668eecb4e9056377f0f632db3b3
Uncle Jee <ujee@UMBAS-MacBook-Air.local>
2023-05-17 17:02:04 +0200

    Home,about, contact Updated version

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.get("/", function (req, res) {
   res.render("home", { startContent: homeStartingContent });
 });
 
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
