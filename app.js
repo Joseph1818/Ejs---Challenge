22c7b713bf8aa5199387ba390b49ad45880ca407
Uncle Jee <ujee@UMBAS-MacBook-Air.local>
2023-05-18 10:37:29 +0200

    last update 18th may

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+var posts = [];
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -21,20 +23,35 @@ app.use(express.static("public"));
 //Creating route
 app.get("/", function (req, res) {
   res.render("home", { startContent: homeStartingContent });
+  // Line bellow console log our array to hyper
+  console.log(posts);
 });
-
+// This methode get the value from app.js and print it to the about.ejs using ejs template value
 app.get("/about", function (req, res) {
   res.render("about", { aboutValue: aboutContent });
 });
-
-app.get("/contact", function(req,res){
+// This methode get the value from app.js and print it to the contact using ejs template value
+app.get("/contact", function (req, res) {
   res.render("contact", { contactValue: contactContent });
-})
-app.get ("/compose", function(req,res){
+});
+// This methode return the value of compose.ejs into the server
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
+// This methode post the values inputed into the Title and post field of the field and return it
+app.post("/compose", function (req, res) {
+  const composeValue = {
+    postTitle: req.body.postTitle,
+    postPostSection: req.body.postPostSection,
+  };
+  // This line of code return it to the server
+  // console.log(composeValue);
+  // This line of code bellow send the value of post into the "posts array declared above".
+  posts.push(composeValue);
 
-
+  // res.redirect send our user back to the home page after
+  res.redirect("/");
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
